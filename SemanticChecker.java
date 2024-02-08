ce0b29a441bcac9acf434e8eee335d9ee065ee21
Landon Connell <landonbconnell2001@gmail.com>
2024-02-07 23:53:59 -0500

    commented SemanticChecker.java

--- SemanticChecker.java
+++ SemanticChecker.java
@@ -12,6 +12,13 @@ public class SemanticChecker {
         scopes = new ArrayDeque<>();
     }
 
+    /**
+     * Iterates over the variables contained in the stack and returns the Variable instance associated with the identifier,
+     * or return null to indicate it hasn't been declared yet.
+     * 
+     * @param identifier the name of the variable being fetched.
+     * @return the Variable object instance whose identifier matches the argument; return 'null' if it hasn't been declared yet.
+     */
     public static Variable getVariable(String identifier) {
         Variable variable = null;
         Iterator<Set<Variable>> scopeIterator = scopes.iterator();
@@ -28,10 +35,22 @@ public class SemanticChecker {
         return variable;
     }
 
+    /**
+     * Checks if any scope in the stack contains a variable with a name matching the 'identifier' argument
+     * 
+     * @param identifier the name of the variable being checked for declaration
+     * @return true if the variable has been declared, false otherwise.
+     */
     public static boolean isInScope(String identifier) {
         return getVariable(identifier) != null;
     }
 
+    /**
+     * Checks if the top-most/current scope on the stack contains a variable with a name matching the 'identifier' argument.
+     * 
+     * @param identifier the name of the variable being checked for declaration in the current scope
+     * @return true if the variable has been declared in the current scope, false otherwise.
+     */
     public static boolean isInCurrentScope(String identifier) {
         boolean isInCurrentScope = false;
         Set<Variable> currentScope = scopes.peekFirst();
@@ -45,14 +64,27 @@ public class SemanticChecker {
         return isInCurrentScope;
     }
 
+    /**
+     * Adds a new Variable instance to the top-most scope in the scope stack, initializing it
+     * with the name and type provided by the arguments.
+     * 
+     * @param identifier the name of the variable being added to the current scope
+     * @param type the type of the variable being added to the current scope (integer/object)
+     */
     public static void addVariableToCurrentScope(String identifier, Type type) {
         scopes.getFirst().add(new Variable(identifier, type));
     }
 
+    /**
+     * Pushes a new scope to the top of the scope stack.
+     */
     public static void pushNewScope() {
         scopes.addFirst(new HashSet<>());
     }
 
+    /**
+     * Pops the most recently added scope off the scope stack.
+     */
     public static void popScope() {
         scopes.pop();
     }
